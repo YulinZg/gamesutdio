5835b8e37d643250ce58d131846694ccab446fca
YulinZg <lucienzhang0314@gmail.com>
2022-02-27 20:15:09 +0800

    finishing logic for moving

---
+++ MASH/Assets/Script/HelicopterController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelicopterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    float moveSpeed = 1.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+
+    private void move()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            gameObject.transform.Translate(Vector3.up * moveSpeed * Time.deltaTime, Space.World);
+        if (Input.GetKey(KeyCode.DownArrow))
+            gameObject.transform.Translate(Vector3.down * moveSpeed * Time.deltaTime, Space.World);
+        if (Input.GetKey(KeyCode.RightArrow))
+            gameObject.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.World);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            gameObject.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime, Space.World);
+
+    }
+}
